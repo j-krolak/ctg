d911c4ef690e708a175233c2a5467f7649a8a51a
PolishFighter <jakubkrolak@protonmail.com>
2023-04-05 21:04:10 +0200

    Add CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
-#include <random>
-#include <vector>
-#include <cmath>
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+
 #include <CellurarGenerator.h>
 
 using namespace std;
@@ -11,53 +12,123 @@ using namespace std;
 #define CELL_L width*height*4
 
 
+void display_help();
 
-int main(){
+int main(int argc, char** argv){
 
-    const int width=600, height=600;
+    int width=300, height=300, points=50; 
+    string output_file = "cellurarTex.png";
+    bool view = false;
 
-    sf::RenderWindow window(sf::VideoMode(width, height), "Cellurar Texture Generator");
+    if(argc >= 2){
+        for(int i = 0; i < argc; i++){
+            if(!strcmp(argv[i], "--help")){
+                display_help();
+                return 0;
+            }
+
+            if(!strcmp(argv[i], "-v")){
+                view = true;
+                continue;
+            }
+
+            if(!strcmp(argv[i], "-w")){
+                if(i + 1 < argc){
+                    width = stoi(argv[i+1]);
+                }
+                continue;
+
+            }
+
+            if(!strcmp(argv[i], "-h")){
+                if(i + 1 < argc){
+                    height = stoi(argv[i+1]);
+                }
+                continue;
+
+            }
+
+            if(!strcmp(argv[i], "-p")){
+                if(i + 1 < argc){
+                    points = stoi(argv[i+1]);
+                }
+                continue;
+
+            }
+
+            if(i == argc-1){
+                output_file = argv[i];
+            }
+
+        }
+    }else{
+        display_help();
+        return 0;
+    }
 
     
+    cout << "Generating...\n";
     CellurarGenerator cellurarGen({width, height});
-    double* values = cellurarGen.generate(100);
-
-
-    sf::Uint8* cellurarStruct = new sf::Uint8[width*height*4];
+    double* values = cellurarGen.generate(points);
+    
+    sf::Uint8* cellurarTex = new sf::Uint8[width*height*4];
     
     sf::Texture texture;
     texture.create(width, height);
 
-    sf::Sprite sprite{texture};
+  
 
     double intensity = 5.;
 
     for(int i = 0; i < CELL_L; i += 4){
         sf::Uint8 val = static_cast<sf::Uint8>(values[i/4]*255);
-        cellurarStruct[i] = val;
-        cellurarStruct[i+1] = val;
-        cellurarStruct[i+2] = val;
-        cellurarStruct[i+3] = 255;
+        cellurarTex[i] = val;
+        cellurarTex[i+1] = val;
+        cellurarTex[i+2] = val;
+        cellurarTex[i+3] = 255;
     } 
 
+    texture.update(cellurarTex);
+    texture.copyToImage().saveToFile(output_file);
+    cout << "Done!\n";
 
 
-    while(window.isOpen()){
-        sf::Event event;
-        while(window.pollEvent(event)){
-            if(event.type == sf::Event::EventType::Closed){
-                window.close();
+
+   
+    if(view){
+        sf::RenderWindow window(sf::VideoMode(width, height), "Cellurar Texture Generator");
+        sf::Sprite sprite{texture};
+
+        while(window.isOpen()){
+            sf::Event event;
+            while(window.pollEvent(event)){
+                if(event.type == sf::Event::EventType::Closed){
+                    window.close();
+                }
             }
-        }
 
-        texture.update(cellurarStruct);
-        window.clear();
-        window.draw(sprite);
-        window.display();
+            window.clear();
+            window.draw(sprite);
+            window.display();
+        }
     }
 
-    delete [] cellurarStruct;
+
+
+
+    delete [] cellurarTex;
 
 
     return 0;
+}
+
+
+void display_help(){
+    cout << R"(Usage: ctg [options] output
+Options:
+	-w <int>	Width of texture
+	-h <int>	Height of texture
+	-p <int>	Number of points
+	-v		View
+	--help	Display this information)" << endl;
 }
